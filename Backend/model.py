b04fe9363250cd56c415a7ee539eb52e1055fd46
Palak887 <24f1002229@ds.study.iitm.ac.in>
2025-10-04 13:21:26 +0530

    model building

--- Backend/model.py
+++ Backend/model.py
@@ -23,3 +23,34 @@ class Quiz(db.Model):
     remarks = db.Column(db.String)
     Chapter_id = db.Column(db.Integer,db.ForeignKey("chapter.id"), nullable=False)
 
+class Questions(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String)
+    question_statement = db.Column(db.String)
+    option1 = db.Column(db.String, nullable=False)
+    option2 = db.Column(db.String, nullable=False)
+    option3 = db.Column(db.String, nullable=False)
+    option4 = db.Column(db.String, nullable=False)
+    correct_option = db.Column(db.String, nullable=False)
+    quiz_id = db.Column(db.Integer,db.ForeignKey("quiz.id"),nullable=False)
+
+
+
+class Scores(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    time_stamp_of_attempt = db.Column(db.Time)
+    total_scored = db.Column(db.Integer,default=00)
+    quiz_id = db.Column(db.Integer,db.ForeignKey("quiz.id"),nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String,nullable=False,unique=True)
+    password = db.Column(db.String,nullable=False)
+    role = db.Column(db.Integer,default=1)
+    full_name = db.Column(db.String,nullable=False)
+    Qualification = db.Column(db.String,nullable=False)
+    dob = db.Column(db.String,nullable=False)
+    scores = db.relationship("Scores",cascade="all,delete",backref="user",lazy=True)
